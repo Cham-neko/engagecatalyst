7fe144af07970c50cc53e89b776953a58709d84f
masaya_mizuno <masaya_mizuno@fringe0123n.local>
2024-11-11 14:26:26 +0900

    GA

--- app.py
+++ app.py
@@ -14,19 +14,40 @@ from janome.tokenizer import Tokenizer
 from collections import Counter  # ワードクラウド作成に必要
 from itertools import combinations
 import streamlit.components.v1 as components
+from bs4 import BeautifulSoup
+import pathlib
+import shutil
+import streamlit as st
 
-# Google Analytics tracking code
-ga_code = """
+GA_ID = "google_analytics"
+GA_SCRIPT = """
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-G2KBPE365L"></script>
-<script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-CDMJD7VYTJ"></script>
+<script id='google_analytics'>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-
-  gtag('config', 'G-G2KBPE365L');
+  gtag('config', 'G-CDMJD7VYTJ');
 </script>
 """
+
+def inject_ga():
+    
+    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+    if not soup.find(id=GA_ID): 
+        bck_index = index_path.with_suffix('.bck')
+        if bck_index.exists():
+            shutil.copy(bck_index, index_path)  
+        else:
+            shutil.copy(index_path, bck_index)  
+        html = str(soup)
+        new_html = html.replace('<head>', '<head>\n' + GA_SCRIPT)
+        index_path.write_text(new_html)
+
+inject_ga()
+
+
 
 # StreamlitにGoogle Analyticsトラッキングコードを埋め込む
 def add_ga_tracking():
